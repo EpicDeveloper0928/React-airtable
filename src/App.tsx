eab75912db497f45770d6b4c3450849c2d1afc8a
Sasha <webcomet928@gmail.com>
2022-12-09 16:23:58 +0200

    Chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route, Routes } from "react-router";
+import { HashRouter } from "react-router-dom";
+import MainLayout from "./layout/main";
+import Home from "./pages/home";
+
+function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="" element={<MainLayout />}>
+          <Route path="home" element={<Home />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
+}
+
+export default App;
